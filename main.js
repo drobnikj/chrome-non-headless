ea0037ef3cece63a62bb97bebb2e74065333666b
JakubDrobnik <drobnik.j@gmail.com>
2018-03-08 12:02:54 +0100

    init

---
+++ main.js
@@ -0,0 +1,19 @@
+// This file will be replaced by the content of the Act2.sourceCode field,
+// we keep this one here just for testing and clarification.
+
+console.log("Hello world from base Docker container for Apify Actor (with Puppeteer and Framebuffer)");
+
+const Apify = require('apify');
+
+Apify.main(async () => {
+    const browser = await Apify.launchPuppeteer({
+        headless: false,
+        executablePath: '/usr/bin/google-chrome',
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto('http://example.com');
+
+    console.log(await page.title());
+});
